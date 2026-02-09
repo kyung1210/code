01651b547d303d82ed4ab88c211689cdff1895c9
kyung1210 <cafewoohyun@naver.com>
2026-02-09 09:58:01 +0900

    .

---
+++ sdream/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+   const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+        // autoplay: {  /* 팝업 자동 실행 */
+        //     delay: 1000,
+        //     disableOnInteraction: true,
+        // }, 선택 다음 컨트롤 물음표 누르면 동시에 주석 처리됨
+        //effect: "fade", /* fade 효과 */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        
+    });//visual_swiper
+
+})//$(document).ready 
